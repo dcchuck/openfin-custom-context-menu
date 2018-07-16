bed7e222519ca1884e747bb5dfc32c414a479ad2
dcchuck <chuck.danielsson@gmail.com>
2018-07-16 11:42:59 -0400

    Small updates, whole document is context menu enabled, show devtools

--- public/main.js
+++ public/main.js
@@ -1,50 +1,38 @@
 /* global fin */
 document.addEventListener('DOMContentLoaded', () => {
-    const ofVersion = document.getElementById('no-openfin');
     if (typeof fin !== 'undefined') {
         init();
     } else {
-        ofVersion.innerText = 'OpenFin is not available - you are probably running in a browser.';
+        console.log('You should launch me on OpenFin!');
     }
 });
 
-function init () {
-    const thisApp = fin.desktop.Application.getCurrent();
-    fin.desktop.System.showDeveloperTools(thisApp.uuid, thisApp.uuid, () => { console.log('yes'); }, (e) => { console.log(e); });
-    const thisWin = fin.desktop.Window.getCurrent();
-    function buttonClickHandler() {
-        thisWin.getState( state => {
-            new fin.desktop.Window({
-                url: 'child.html',
-                name: Math.random().toString(),
-                autoShow: true,
-                state: state
-            });
-        });
-    }
+function init() {
 
+    // Create the Context Menu window on launch, but do not show it.
+    const contextMenu = new fin.desktop.Window({
+        url: 'menu.html',
+        name: Math.random().toString(),
+        autoShow: false,
+        saveWindowState: false,
+        frame: false,
+        defaultHeight: 66,
+        defaultWidth: 200,
+        cornerRounding: {
+            height: 3,
+            width: 3
+        },
+        smallWindow: true
+    });
+
+    // The context menu handler shows the window where the user right clicked
     function contextMenuHandler() {
         fin.desktop.System.getMousePosition(mousePosition => {
-            new fin.desktop.Window({
-                url: 'menu.html',
-                name: Math.random().toString(),
-                autoShow:true,
-                defaultTop: mousePosition.top,
-                defaultLeft: mousePosition.left,
-                saveWindowState: false,
-                frame: false,
-                defaultHeight: 100,
-                defaultWidth: 200,
-                cornerRounding: {
-                    height: 3,
-                    width: 3
-                }
+            contextMenu.showAt(mousePosition.left, mousePosition.top, false, () => {
+                contextMenu.bringToFront(() => contextMenu.focus());
             });
         });
     }
 
-    const launchButton = document.getElementById('openWin');
-    const childWindowAnchor = document.getElementById('childWindowAnchor');
-    launchButton.onclick = buttonClickHandler;
-    childWindowAnchor.addEventListener('contextmenu', contextMenuHandler);
+    document.addEventListener('contextmenu', contextMenuHandler);
 }
